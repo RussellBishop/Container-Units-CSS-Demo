fc9e1445d8086d73bf02972f4943f6607538836b
Russell Bishop <hey@russellbishop.co.uk>
2017-12-03 23:01:26 +0000

    Setup is coming in

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en" class="u-height-100">
+<head>
+    <meta charset="UTF-8">
+    <title>SCSS Playground</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/dist/css/main.css">
+</head>
+<body class="u-bgcolor-purple u-flex u-ai-center u-jc-center u-height-100">
+
+    <div class="o-container">
+
+        <h1>Container Units CSS</h1>
+
+        <div class="u-flex u-jc-between u-height-80vh">
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+            <div class="u-columnspan-1 u-bgcolor-burn" style="mix-blend-mode: hard-light;">
+
+            </div>
+
+        </div>
+
+        <div class="c-speech u-ph-c-1 u-pv-r-05 u-bgcolor-aqua u-color-purple u-radius-10px">
+            Your container’s width is set as a CSS variable
+        </div>
+
+    </div>
+
+</body>
+</html>
